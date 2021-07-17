d3429d2b65443579545e185bddf50dee8b186f8d
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2021-07-17 19:13:37 +0200

    Changed class name and bean access modifier

--- src/main/java/stevenjdh/covidtracker/AppConfig.java
+++ src/main/java/stevenjdh/covidtracker/AppConfig.java
@@ -23,10 +23,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class AppConfig {
     
     @Bean
-    HttpClient httpClient(){
+    public HttpClient httpClient(){
         return HttpClient.newHttpClient();
     }
 }
